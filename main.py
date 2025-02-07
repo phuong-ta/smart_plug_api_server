405e54d646f8d30395c82925c07e6f936a4d7153
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-02-07 12:31:41 +0200

    test db connection

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from typing import Optional
+from contextlib import asynccontextmanager
+import asyncio
+import os
 
 from fastapi import FastAPI
+from databases import Database
+
 from routers.price_router import price_router
 from routers.booking_router import booking_router
-#from .routers import electric_price
+
+DATABASE_URL = os.getenv("DB_INTERNAL_URL")
+database = Database(DATABASE_URL)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        # Connect to the database asynchronously
+        await database.connect()
+        print("Connected to the database")
+        yield
+    except Exception as e:
+        print(f"Error while connecting: {e}")
+    finally:
+        # Disconnect from the database asynchronously
+        await database.disconnect()
+        print("Disconnected from the database")
 
 
+app = FastAPI(lifespan=lifespan)
 
-app = FastAPI()
 app.include_router(price_router)
 app.include_router(booking_router)
-#app.include_router(electric_price.price_router)
 
 
 @app.get("/")
