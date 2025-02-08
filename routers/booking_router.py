8d809996a5391c3f7d6703f851dd8e377f66eec4
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-02-08 12:33:53 +0200

    build new crud 2

--- routers/booking_router.py
+++ routers/booking_router.py
@@ -19,7 +19,7 @@ def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 # Get a specific booking by ID
 @booking_router.get("/{booking_id}", response_model=schemas.Booking)
 def read_booking(booking_id: int, db: Session = Depends(get_db)):
-    db_booking = booking_crud.get_booking(db, booking_id=booking_id)
+    db_booking = booking_crud.get_booking_by_id(db, booking_id=booking_id)
     if db_booking is None:
         raise HTTPException(status_code=404, detail="Booking not found")
     return db_booking
