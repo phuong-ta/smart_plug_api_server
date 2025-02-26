c1aa9f176c0d46add730255def27eaf627ca639d
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-02-26 09:04:59 +0200

    modify table

--- db/energy/models.py
+++ db/energy/models.py
@@ -9,10 +9,10 @@ class EnergyReport(Base):
     __tablename__ = 'energy_report'
 
     id = Column(Integer, primary_key=True, index=True)
-    charger_id = Column(Integer, index=True)  # Assuming charger_id is a string
+    charger_id = Column(Integer, index=True) 
     start_time = Column(DateTime)
     end_time = Column(DateTime)
-    energy_consume = Column(Float)  # Correct declaration
+    energy_consume = Column(Float)  
     price = Column(Float)
 
 # Function to create table
