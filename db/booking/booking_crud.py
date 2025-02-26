19c5d5984f657e2af7091c1f618be0e5bd8e6856
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-02-26 09:29:43 +0200

    modify table

--- db/booking/booking_crud.py
+++ db/booking/booking_crud.py
@@ -37,7 +37,7 @@ def delete_booking(db: Session, booking_id: int):
     return db_booking
 
 def get_newest_booking_by_id(db: Session, charger_id: int):
-    return db.query(models.Booking).filter(models.Booking.id == charger_id).order_by(desc(models.Booking.start_time)).first()
+    return db.query(models.Booking).filter(models.Booking.charger_id == charger_id).order_by(desc(models.Booking.start_time)).first()
 
 def get_nearest_booking_by_charger_id(db: Session, charger_id: int):
     return db.query(models.Booking)\
