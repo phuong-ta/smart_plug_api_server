a0498a5adf2753ad824f44f8164b75e6a7ad585b
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-02-26 12:32:34 +0200

    fix cors

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from fastapi import FastAPI
 from routers.price_router import price_router
 from routers.booking_router import booking_router
 from routers.energy_report_router import energy_router
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import database
 from db.energy.models import EnergyReport
 from db.booking.models import Booking
 
+
+# Define allowed origins
+origins = [
+    "http://localhost:3000",  # React local dev server
+    #"https://yourfrontenddomain.com",  # Your production frontend URL
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -28,6 +35,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows only the listed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(price_router)
 app.include_router(booking_router)
