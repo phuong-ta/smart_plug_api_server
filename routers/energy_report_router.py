2a78a1a7002158036e6b5a97b5ef98fba5b7f388
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-03-08 12:19:23 +0200

    correct get monthy energy

--- routers/energy_report_router.py
+++ routers/energy_report_router.py
@@ -5,6 +5,9 @@ from db.database import get_db
 from db.energy import energy_curd
 from db.energy import schemas
 
+from datetime import datetime
+
+
 energy_router = APIRouter(prefix="/energy_reports", tags=["energy_reports"])
 
 # Create a new energy report
@@ -39,7 +42,25 @@ def read_reports_by_charger_id(charger_id: int, db: Session = Depends(get_db)):
 # Get a specific energy report by ID
 @energy_router.get("/energy_consumption/{charger_id}", response_model=list[schemas.EnergyReport])
 def count_monthly_energy(charger_id: int, db: Session = Depends(get_db)):
-    db_report = energy_curd.get_monthly_energy_consumption_by_id(db, charger_id=charger_id)
-    if db_report is None:
+    charger_report = energy_curd.get_reports_by_charger_id(db, charger_id=charger_id)
+    if charger_report is None:
         raise HTTPException(status_code=404, detail="Energy Report not found")
-    return db_report
+
+    # Get the current month and year
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+
+    # Initialize total energy consumption for the current month
+    total_energy_current_month = 0
+
+    # Iterate over each record in the data
+    for record in charger_report:
+        # Parse the start time to a datetime object
+        start_time = datetime.fromisoformat(record["start_time"])
+
+        # Check if the record is in the current month and year
+        if start_time.year == current_year and start_time.month == current_month:
+            # Add the energy consumption to the total
+            total_energy_current_month += record["energy_consume"]
+
+    return total_energy_current_month
