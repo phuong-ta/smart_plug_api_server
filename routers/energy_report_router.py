33ce490b9956a9926b888e540fecfab7adfa565c
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-03-08 12:35:14 +0200

    modify funtion

--- routers/energy_report_router.py
+++ routers/energy_report_router.py
@@ -1,19 +1,20 @@
-from sqlalchemy.orm import Session
-from sqlalchemy import desc
+from datetime import datetime
+
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
 from db.database import get_db
 from db.energy import energy_curd
 from db.energy import schemas
 
-from datetime import datetime
-
-
 energy_router = APIRouter(prefix="/energy_reports", tags=["energy_reports"])
+
 
 # Create a new energy report
 @energy_router.post("/", response_model=schemas.EnergyReport)
 def create_energy_report(report: schemas.EnergyReportCreate, db: Session = Depends(get_db)):
     return energy_curd.create_energy_report(db=db, report=report)
+
 
 # Get all energy reports
 @energy_router.get("/", response_model=list[schemas.EnergyReport])
@@ -30,7 +31,6 @@ def read_energy_report(report_id: int, db: Session = Depends(get_db)):
     if db_report is None:
         raise HTTPException(status_code=404, detail="Energy Report not found")
     return db_report
-
 
 
 # Get reports by charger ID
@@ -50,17 +50,11 @@ def read_reports_by_charger_id(charger_id: int, db: Session = Depends(get_db)):
     current_month = datetime.now().month
     current_year = datetime.now().year
 
-    # Initialize total energy consumption for the current month
-    total_energy_current_month = 0
+    total_energy = sum(
+        entry["energy_consume"]
+        for entry in charger_report
+        if datetime.fromisoformat(entry["start_time"]).year == current_year
+        and datetime.fromisoformat(entry["start_time"]).month == current_month
+    )
 
-    # Iterate over each record in the data
-    for record in charger_report:
-        # Parse the start time to a datetime object
-        start_time = datetime.fromisoformat(record["start_time"])
-
-        # Check if the record is in the current month and year
-        if start_time.year == current_year and start_time.month == current_month:
-            # Add the energy consumption to the total
-            total_energy_current_month += record["energy_consume"]
-
-    return total_energy_current_month
+    return total_energy
