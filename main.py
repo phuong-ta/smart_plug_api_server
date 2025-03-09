95af21b9361497dea4acf2ffafe30887505fee6e
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2025-03-09 09:10:02 +0200

    create new database

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from routers.booking_router import booking_router
 from routers.energy_report_router import energy_router
 from routers.price_router import price_router
 
+from db.booking.models import Booking
+from db.energy.models import EnergyReport
+
 # Define allowed origins
 origins = [
     "http://localhost:3000",  # React local dev server
@@ -20,8 +23,8 @@ async def lifespan(app: FastAPI):
     try:
         # Connect to the database asynchronously
         await database.connect()
-        # Booking.create_table()
-        # EnergyReport.create_energy_table()
+        Booking.create_table()
+        EnergyReport.create_energy_table()
         print("Connected to the database")
         yield
     except Exception as e:
